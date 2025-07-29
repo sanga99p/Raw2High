569fde69db1c29f3b0515282e7405cf350eba8c0
sanga99p <sangapark99@gmail.com>
2025-07-29 16:24:28 +0900

    upload NOD

--- Preprocessing_DATASET/ROD/ROD_dataset_download.py
+++ Preprocessing_DATASET/ROD/ROD_dataset_download.py
@@ -1,6 +1,8 @@
 import openi
-# openi.download_file(repo_id="innovation_contest/innov202305091731448",file="00Train-raws.zip",cluster="NPU",save_path="/media/ssd1/sanga/RAW2High/DATASET/ROD",force=False)
 
+# Download the dataset |  for annoation : https://nullmax-vision.github.io/
+
+openi.download_file(repo_id="innovation_contest/innov202305091731448",file="00Train-raws.zip",cluster="NPU",save_path="/media/ssd1/sanga/RAW2High/DATASET/ROD",force=False)
 openi.download_file(repo_id="innovation_contest/innov202305091731448",file="01Valid-raws.zip",cluster="NPU",save_path="/media/ssd1/sanga/RAW2High/DATASET/ROD",force=False)
 
 
