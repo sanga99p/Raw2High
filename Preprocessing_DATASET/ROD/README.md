5c812909df9474a765939e9890f445b9e3f4c46e
sanga99p <sangapark99@gmail.com>
2025-07-29 16:10:05 +0900

    upload

---
+++ Preprocessing_DATASET/ROD/README.md
@@ -0,0 +1,8 @@
+
+## data_utils.py 
+
+modules used for data pre-processing
+
+## daataset.py 
+
+definition of dataloader
---
+++ ROD_dataset_download.py
@@ -0,0 +1,6 @@
+import openi
+# openi.download_file(repo_id="innovation_contest/innov202305091731448",file="00Train-raws.zip",cluster="NPU",save_path="/media/ssd1/sanga/RAW2High/DATASET/ROD",force=False)
+
+openi.download_file(repo_id="innovation_contest/innov202305091731448",file="01Valid-raws.zip",cluster="NPU",save_path="/media/ssd1/sanga/RAW2High/DATASET/ROD",force=False)
+
+
